8496415cc0e633d899d62fe75bc51dd3a255ef41
ehime <dodomeki@gmail.com>
2014-04-24 11:03:53 -0700

    Really bad naming day i guess

--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php
-
 error_reporting(-1);
 ini_set('display_errors', 1);
 
 define('APP_PATH', dirname(__DIR__));
-require APP_PATH . '/vendor/autoload.php';
+define('ENV_FILE', APP_PATH . '/config/' . (getenv('APP_ENV') ?: 'local') . '.json');
+
+require APP_PATH . '/src/Helpers/LazyLoader.php';
+
+$lazyLoader = New \Helpers\LazyLoader(APP_PATH . '/src/');
+$lazyLoader->registerGenericNamespace('Helpers');
+
+$payload =
+    [
+        'type' => (! isset($argv) ?: 0),
+        'args' => (! isset($argv) ? $_GET : $argv),
+    ];
 
 $bootstrap = New \Helpers\Bootstrap($payload);
 $bootstrap->run();
